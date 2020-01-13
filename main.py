9532d6cb9fc006cb4173b4aead824957270feb7c
giefferre <gianfranco.reppucci@gmail.com>
2020-01-14 00:22:27 +0100

    update main to use converter package instead of mock data

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-from flask import Flask, request, jsonify, abort, escape
+from flask import Flask, request, jsonify
 from werkzeug.exceptions import BadRequest
+from converter import Converter, ConverterRequest, ConverterResponse
+from converter.rate_providers import RandomRateProvider
+
+
+'''
+The following inizialization could happen elsewhere and the dependency converter
+could be passed using dependency injection, using tools such as
+https://pypi.org/project/Flask-Injector/
+'''
+rp = RandomRateProvider()
+c = Converter(rp)
 
 app = Flask(__name__)
 
-
 @app.route('/convert', methods=['GET'])
 def convert():
-    return jsonify({
-        "amount": 12.34,
-        "currency": "EUR"
-    })
+    try:
+        req = ConverterRequest(
+            float(request.args.get('amount')),
+            request.args.get('src_currency'),
+            request.args.get('dest_currency'),
+            request.args.get('reference_date'),
+        )
+    except Exception as e:
+        raise BadRequest(e)
+
+    res = c.convert(req)
+
+    return jsonify(res.serialize())
 
 
 if __name__ == '__main__':
